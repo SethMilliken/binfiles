ea5a9943695519f909b52d3f6f976ca5657f98e2
milliken <devnull@localhost>
2006-08-18 02:06:14 +0000

    simple script to open a zlog journal, or append to existing one with timestamp.

---
+++ journal.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+DATE=`date '+%A, %B %d, %Y, %T %Z'`
+TIME=`date '+%T'`
+LOCATION=${HOME}/sandbox/personal/zlog
+FILENAME=`date +%Y-%m-%d`.txt 
+FILE=${LOCATION}/${FILENAME}
+if [ ! -f ${FILE} ] 
+then
+	touch ${FILE}
+	echo ${DATE}\\n >> ${FILE}
+	svn add ${FILE}
+else
+	echo \\n${TIME}\\n >> ${FILE}
+fi
+# script to go to end of file automatically
+${EDITOR} -c $ ${FILE}
+#${edITOR} ${FILE} << EOF
