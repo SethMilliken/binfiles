84c08d248a4ce06e8c33363fd2f608b875012d56
milliken <devnull@localhost>
2007-01-11 23:52:05 +0000

    adding more stuff here

--- svnstatus.sh
+++ svnstatus.sh
@@ -2,3 +2,5 @@
 svn -q st ${HOME}
 svn st ${HOME}/sandbox/personal
 svn -q st ${HOME}/sandbox/code
+svn -q st ${HOME}/sandbox/backup/doing/
+svn up '/Users/milliken/Library/Application Support/Adium 2.0/Users/Default/Logs/'
