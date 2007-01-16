dab7a89badab16a808fded833d7edc84a3bf3847
milliken <devnull@localhost>
2007-01-16 22:31:49 +0000

    script for restarting growl. to bind to hotkey.

---
+++ restart_growl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+GROWLAPP=/Users/sethm/Library/PreferencePanes/Growl.prefPane/Contents/Resources/GrowlHelperApp.app
+NOTIFICATION_SCRIPT=/usr/local/bin/growlnotify
+killall -HUP GrowlHelperApp
+open ${GROWLAPP}
+sleep 1
+${NOTIFICATION_SCRIPT} 'Growl' -m'Restarted'
