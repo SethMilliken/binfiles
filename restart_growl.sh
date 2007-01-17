c3c5238912dc7fd6a76208bfe69b6ff26a6d0397
milliken <devnull@localhost>
2007-01-17 20:57:44 +0000

    adjust sleep interval

--- restart_growl.sh
+++ restart_growl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-GROWLAPP=/Users/sethm/Library/PreferencePanes/Growl.prefPane/Contents/Resources/GrowlHelperApp.app
+GROWLAPP=${HOME}/Library/PreferencePanes/Growl.prefPane/Contents/Resources/GrowlHelperApp.app
 NOTIFICATION_SCRIPT=/usr/local/bin/growlnotify
 killall -HUP GrowlHelperApp
 open ${GROWLAPP}
-sleep 1
+sleep 2
 ${NOTIFICATION_SCRIPT} 'Growl' -a'GrowlHelperApp' -m'Restarted'
