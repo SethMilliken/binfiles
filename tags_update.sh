2f454298e4f951fa44c43a44370d8ba535d3dd30
milliken <devnull@localhost>
2007-07-31 17:38:30 +0000

    latest

--- tags_update.sh
+++ tags_update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #TAG_ROOT=
-find . -name '*.txt' -print0 | xargs -J % -0 -n1 echo | sed -e 's/\(\(.*\)\/\(.*\).txt\)/\3	\1	\/^/' > tags
+find . -name '*.txt' -print0 | xargs -J % -0 -n1 echo | sed -e 's/\(\(.*\)\/\(.*\).txt\)/\3	\1	\/^/' | sort > tags
