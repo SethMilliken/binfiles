aef86e50ec903029d6499c640f4d6ac993d36347
milliken <devnull@localhost>
2009-05-22 00:11:02 +0000

    updated to deal with new structure dictated by dovcot Maildir

--- update_mail_links.sh
+++ update_mail_links.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/local/bin/bash
 function previous_month {
 	PREV_MONTHS="12 01 02 03 04 05 06 07 08 09 10 11"
 	PREV_MONTH=`echo $PREV_MONTHS | awk '{print $'"${1}"'}'`
@@ -8,15 +8,17 @@ MONTH=`date +%m`
 DATE=${YEAR}-${MONTH}
 previous_month `date +%m`
 PREVIOUS_DATE=${YEAR}-${PREV_MONTH}
+#DEBUG="echo "
+DEBUG=""
 
 function update_path {
 	CURRENT_LINK=$1
 	MATCH_STRING=$2
-	if [[ -L ${CURRENT_LINK} ]]
+	if [ -L ${CURRENT_LINK} ]
 	then
 		FULLPATH=`ls -l ${CURRENT_LINK} | awk '{print $11}'`
-		DIRECTORY=`dirname ${FULLPATH}`
-		NEW_TARGET=`ls ${DIRECTORY}/${MATCH_STRING}*`
+		CANDIDATE=`echo $FULLPATH | sed -e "s/\(.*\.\)\([0-9]*-[0-9]*\)\(.*\)/\1${MATCH_STRING}\3/"`
+		NEW_TARGET=`ls -d ${CANDIDATE}`
 		if [[ ${NEW_TARGET} != "" && -e ${NEW_TARGET} ]]
 		then
 			if [[ $NEW_TARGET == $FULLPATH ]]
@@ -25,7 +27,7 @@ function update_path {
 				continue
 			else
 				echo "New target for " ${CURRENT_LINK} ": " $NEW_TARGET
-				ln -shf ${NEW_TARGET} ${CURRENT_LINK}
+				${DEBUG} ln -shf ${NEW_TARGET} ${CURRENT_LINK}
 				continue
 			fi
 		else
@@ -33,15 +35,42 @@ function update_path {
 			continue
 		fi
 	else
-		echo "Not a symbolic link: " ${CURRENT_LINK}
+		#echo "Not a symbolic link: " ${CURRENT_LINK}
 		continue
 	fi
 }
 
-for link in `ls -1`
+function contains {
+	TARGET=$1
+	MATCH=$2
+	RESULT=`echo $TARGET | sed -e "/$MATCH/g"`
+	if [[ $RESULT != "" ]]
+	then
+		return 0
+	else
+		return 1
+	fi
+}
+	
+
+for link in `ls -1d .*`
 do
 	# A couple of exceptions
-	if [[ $link == "previous" ]]
+	if [[ $link == "." ]]
+	then
+		continue
+	fi
+	if [[ $link == ".." ]]
+	then
+		continue
+	fi
+	if [[ $link == ".current.in" ]]
+	then
+		continue
+	fi
+	contains $link "prev"
+	VALUE=$?
+	if [[ $VALUE -eq 1 ]]
 	then
 		update_path $link ${PREVIOUS_DATE} 
 		continue
