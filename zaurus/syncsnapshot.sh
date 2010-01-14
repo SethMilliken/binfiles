4514e777c292134db8069bf404665d36434521e2
milliken <devnull@localhost>
2010-01-14 13:06:28 +0000

    latest

--- zaurus/syncsnapshot.sh
+++ zaurus/syncsnapshot.sh
@@ -2,6 +2,7 @@
 # automatically mount ftp filesystem somehow...
 # ftp://ftp.openbsd.org/pub/OpenBSD/snapshots/zaurus/
 SOURCE="/Volumes/ftp.openbsd.org/"
-DESTINATION="${HOME}/staging/zaurus/snapshot/"
+#DESTINATION="${HOME}/staging/zaurus/snapshot/"
+DESTINATION="./snapshot/"
 rsync -avz --cvs-exclude --exclude=.DS_Store --progress "${SOURCE}" "${DESTINATION}"
 
