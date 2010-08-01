16a3407cb369aa32f757b4a36f61f654f36b24aa
milliken <devnull@localhost>
2010-08-01 20:02:40 +0000

    Script to uniquely identify copies of MacVim.

---
+++ VimFix.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This is a script for modifying copies of MacVim to uniquely identify them.
+
+function vimfix {
+	ID=$1
+	NAME=$2
+	PATH=$3
+	ICON=$4
+	/usr/libexec/PlistBuddy ${PATH}/Info.plist -c 'Set :CFBundleIdentifier org.vim.MacVim.'${NAME}
+	/usr/libexec/PlistBuddy ${PATH}/Info.plist -c 'Set :CFBundleName '${NAME}
+	/usr/libexec/PlistBuddy ${PATH}/Info.plist -c 'Set :CFBundleSignature '${ID}
+	/usr/bin/sed -i '' -es/VIMM/${ID}/ ${PATH}/PkgInfo
+	/bin/cp ~/Pictures/VimIcons/${ICON} ${PATH}/Resources/MacVim.icns
+}
+
+vimfix VIM7 SourceCode /Applications/SourceCode.app/Contents VimBlue.icns
+vimfix VIM6 Research /Applications/Research.app/Contents VimGrey.icns
+vimfix VIM5 Tasks /Applications/Tasks.app/Contents VimPurple.icns
+vimfix VIM4 Vimwiki /Applications/Vimwiki.app/Contents VimPaleGreen.icns
