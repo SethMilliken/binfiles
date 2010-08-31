4dde6fcbdb1cc995625f83d58bc3699a9d09bd5f
milliken <devnull@localhost>
2010-08-31 22:01:03 +0000

    Cleaned up notification.

--- firefox_toggle.sh
+++ firefox_toggle.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-NOTIFICATION_SCRIPT=/usr/local/bin/growlnotify
-if ps ax | grep firefox-bin | awk '{print $3}' | grep T
+#
+# firefox_toggle
+#
+# Suspends and resumes all Firefox instances.
+# Posts a growl notification to indicate status.
+#
+GROWLNOTIFY=/usr/local/bin/growlnotify
+FIREFOX_NAME="Firefox 3.6.app"
+FIREFOX_PROCESS="firefox-bin"
+if ps ax | grep ${FIREFOX_PROCESS} | awk '{print $3}' | grep T
 then
-	killall -CONT firefox-bin
+	killall -CONT ${FIREFOX_PROCESS}
 	RESULT="Thawed"
 else
-	killall -STOP firefox-bin
+	killall -STOP ${FIREFOX_PROCESS}
 	RESULT="Frozen"
 fi
-${NOTIFICATION_SCRIPT} 'Firefox' -a'Firefox' -m${RESULT}
+${GROWLNOTIFY} --identifier $0 -t 'Firefox' -a "${FIREFOX_NAME}" -m ${RESULT}
