e50b0281f8a09b264341c62285f6e6abb452be27
Seth Milliken <seth@araxia.net>
2010-12-07 15:35:25 -0800

    restart_growl now acts as a toggle.

--- restart_growl.sh
+++ restart_growl.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 GROWLAPP=/Library/PreferencePanes/Growl.prefPane/Contents/Resources/GrowlHelperApp.app
+GROWLMENU=/Library/PreferencePanes/Growl.prefPane/Contents/Resources/GrowlMenu.app/Contents/MacOS/GrowlMenu
 NOTIFICATION_SCRIPT=/usr/local/bin/growlnotify
-killall -HUP GrowlHelperApp
-open ${GROWLAPP}
-sleep 1
-${NOTIFICATION_SCRIPT} 'Growl' -a'GrowlHelperApp' -m'Restarted'
+
+function running {
+	TARGET=$1
+	MATCH=$2
+	RESULT=`ps x | grep Growl | grep -v grep`
+	if [[ $RESULT != "" ]]
+	then
+		killall -m -HUP Growl
+	else
+		open ${GROWLAPP}
+		sleep 1
+		${NOTIFICATION_SCRIPT} 'Growl' -a'GrowlHelperApp' -m'Restarted'
+		${GROWLMENU} &
+	fi
+}
+
+running
