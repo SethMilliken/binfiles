ed6abef8598f0709b6a5c0d9d01e37c7aaae0897
Seth Milliken <seth@araxia.net>
2011-01-06 15:14:41 -0800

    now correctly copies to clipboard AND to stdout - growl message corrected through IFS reset - fix long timestamp format - retab

--- timestamp.sh
+++ timestamp.sh
@@ -1,23 +1,25 @@
-#!/bin/sh
-# TODO: figure out what is wrong with 1 format
+#!/bin/bash
 function timestamp {
-	case $1 in
-		 1)
-		     FORMAT='+%Y-%m-%d\ %H:%M:%S\ %Z'
-			 ;;
-		 *)
-		     FORMAT='+%Y-%m-%d'
-			 ;;
-	esac
+    case $1 in
+         1)
+             FORMAT='%Y-%m-%d %H:%M:%S %Z'
+             ;;
+         *)
+             FORMAT='%Y-%m-%d'
+             ;;
+    esac
 
-	TIMESTAMP=`date ${FORMAT}`
-	echo ${TIMESTAMP} | pbcopy
-	growlnotify --identifier timestamp -a 'Terminal.app' -t copy -m "on clipboard: '"${TIMESTAMP}"'"
+    TIMESTAMP=$(date +"${FORMAT}")
+    echo $TIMESTAMP | tee >(pbcopy)
+    OLDIFS=$IFS
+    IFS=    \
+
+    $(which growlnotify) --identifier timestamp -a 'Terminal.app' -t copy -m "on clipboard: '"${TIMESTAMP}"'"
+    IFS=$OLDIFS
 }
 
-function run {	
-	timestamp $1
-	exit 0
+function run {
+    timestamp $1
+    exit 0
 }
 run $@
-
