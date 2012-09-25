3e66fc81c46e72faf38191a5f083cb41c0d57fde
Seth Milliken <seth@janrain.com>
2012-09-25 12:52:57 -0700

    vimstart cleanup

--- vimstart.rb
+++ vimstart.rb
@@ -29,8 +29,6 @@ class Command
   end
 end
 class CommandList
-  @list
-
   def initialize()
     @list = {}
   end
@@ -52,22 +50,25 @@ end
 
 # Define Commands
 commandList = CommandList.new
-commandList.insert(Command.new("tasks",     "Opens taskstack in Tasks instance of MacVim", "tvim ~/sandbox/work/janrain.tst.txt"))
-commandList.insert(Command.new("work",      "Open personal taskstack.", "tvim ~/sandbox/personal/todo/personal.tst.txt"))
-commandList.insert(Command.new("v",         "Open this script.", "svim ~/bin/vimstart.rb"))
-commandList.insert(Command.new("vimrc",     "Open .vimrc.", "svim ~/.vimrc"))
-commandList.insert(Command.new("vimhelp",   "Open vimhelp.", "open /Applications/VimHelp.app"))
-commandList.insert(Command.new("scratch",   "Open scratch.", "open -a /Applications/Scratch.app ~/.vim/swap/scratch.scratch"))
-commandList.insert(Command.new("wiki",   "Open vimwiki.", "wvim ~/sandbox/work/wiki/SethMilliken.wiki"))
-commandList.insert(Command.new("todo",   "Open todo.", "dvim ~/sandbox/personal/todo/todo.txt"))
-commandList.insert(Command.new("ff",   "Open default FireFox.", "/Applications/Firefox.app/Contents/MacOS/firefox -P default"))
-commandList.insert(Command.new("ffre",   "Open FireFoxResearch.", "/Applications/FireFoxResearch.app/Contents/MacOS/firefox -P Research"))
-commandList.insert(Command.new("ffvo",   "Open volatile FireFox.", "/Applications/FireFoxVolatile.app/Contents/MacOS/firefox -P Minefield"))
-commandList.insert(Command.new("ffdev",   "Open dev FireFox.", "/Applications/FireFoxDactyl.app/Contents/MacOS/firefox --chromebug -P PentadactylDev"))
-commandList.insert(Command.new("ffvimp",   "Open vimperator FireFox.", "/Applications/FireFoxTest.app/Contents/MacOS/firefox --chromebug -P VimperatorTest"))
-commandList.insert(Command.new("fftest-choose",   "Open FireFoxTest.", "/Applications/FireFoxTest.app/Contents/MacOS/firefox -ProfileManager"))
-commandList.insert(Command.new("fftest",   "Open FireFoxTest.", "/Applications/FireFoxTest.app/Contents/MacOS/firefox -P Test"))
-commandList.insert(Command.new("ffchoose",   "Open selected FireFox.", "/Applications/FireFox.app/Contents/MacOS/firefox --chromebug -ProfileManager"))
+commandList.insert(Command.new("tasks"         , "Opens taskstack in Tasks instance of MacVim" , "tvim ~/sandbox/work/janrain.tst.txt"))
+commandList.insert(Command.new("work"          , "Open personal taskstack."                    , "tvim ~/sandbox/personal/todo/personal.tst.txt"))
+commandList.insert(Command.new("v"             , "Open this script."                           , "svim ~/bin/vimstart.rb"))
+commandList.insert(Command.new("vimrc"         , "Open .vimrc."                                , "svim ~/.vimrc"))
+commandList.insert(Command.new("vimhelp"       , "Open vimhelp."                               , "open /Applications/VimHelp.app"))
+commandList.insert(Command.new("scratch"       , "Open scratch."                               , "open -a /Applications/Scratch.app ~/.vim/swap/scratch.scratch"))
+commandList.insert(Command.new("wiki"          , "Open vimwiki."                               , "wvim ~/sandbox/work/wiki/SethMilliken.wiki"))
+commandList.insert(Command.new("todo"          , "Open todo."                                  , "dvim ~/sandbox/personal/todo/todo.txt"))
+
+commandList.insert(Command.new("ffvo"          , "Open volatile FireFox."   , "/Applications/FireFoxVolatile.app/Contents/MacOS/firefox -P Minefield"))
+commandList.insert(Command.new("ffdev"         , "Open dev FireFox."        , "/Applications/FireFoxDactyl.app/Contents/MacOS/firefox --chromebug -P PentadactylDev"))
+commandList.insert(Command.new("ffvimp"        , "Open vimperator FireFox." , "/Applications/FireFoxTest.app/Contents/MacOS/firefox --chromebug -P VimperatorTest"))
+commandList.insert(Command.new("ffchoose"      , "Open selected FireFox."   , "/Applications/FireFox.app/Contents/MacOS/firefox --chromebug -ProfileManager"))
+commandList.insert(Command.new("fftest-choose" , "Open FireFoxTest."        , "/Applications/FireFoxTest.app/Contents/MacOS/firefox -ProfileManager"))
+
+commandList.insert(Command.new("ff"            , "Open default FireFox."    , "/Applications/Firefox.app/Contents/MacOS/firefox -P default"))
+commandList.insert(Command.new("ffre"          , "Open FireFoxResearch."    , "/Applications/FireFoxResearch.app/Contents/MacOS/firefox -P documentation"))
+commandList.insert(Command.new("fftrack"       , "Open FireFoxTrack."       , "/Applications/FireFoxTrack.app/Contents/MacOS/firefox -P track"))
+commandList.insert(Command.new("fftest"        , "Open FireFoxTest."        , "/Applications/FireFoxTest.app/Contents/MacOS/firefox -P testing"))
 
 # Parse Options
 # TODO: Actually use --instance
